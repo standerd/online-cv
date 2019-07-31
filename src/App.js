9a96a9459aa906293e59b5cd19cf6eedc30975fd
Dewald <Dewald@23-227-105-79.north.dsl.telkomsa.net>
2019-07-31 09:28:28 +0200

    first draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Details from "./containers/details/details"
+import About from "./containers/about/about"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="about">
+      <About/>
+      </div>
+
+      <div className="details">
+      <Details/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/containers/details/details.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "../../App.css"
+
+const details = () => (
+    <div>
+        <h1><i class="far fa-address-card"></i> About Me</h1>
+        <p>I have spent my first 17 years of my working career in a financial role. I have subsequently spent 8 months <br></br> developing my skills as a FULL STACK WEB DEVELOPER.</p>
+        <p>I have developed a broad range of skills in web development, and this together with my vast business and <br></br>management experience I trust will put my skillset in a category of its own.</p>
+
+        <h1><i class="fas fa-briefcase"></i> Work Experience</h1>
+        <p>2001-2005: Auditing Articles at Smith Kruger Auditors in Kroonstad - Full Function Auditing and Accounting<br></br></p>
+        <p>2005-2006: Auditing Articles at JP McDonald Auditors in Kempton Park - Full Function Auditing and Accounting<br></br></p>
+        <p>2006-2019: Value Logistics Limited<br></br></p>
+        <ul>
+            <li>Highest position achieved - Financial Manager reporting to the Financial Director</li>
+            <li>Management Experience - Managed the Insurance and Accounts Payable departments with 23 staff <br></br>reporting to me at the time</li>
+            <li>Experience gained - 
+                <ul>
+                    <li>Management accounts preperation</li>
+                    <li>Director operational reporting</li>
+                    <li>Full Insurance function - FAIS Representative and Key Individual for FSP</li>
+                    <li>Managing of the full Accounts Payable function. > 1000 Vendors and > R 100m monthly <br></br>payments monthly</li>
+                    <li>Exposure to SQL and Company databases for operation reporting</li>
+                </ul>
+            </li>
+            <li></li>
+        </ul>
+
+        <h1> <i class="fas fa-clipboard-check"></i> Personal Attributes</h1>
+        <p>Dedicated<br></br></p>
+        <p>Work well in teams<br></br></p>
+        <p>Great attention to detail<br></br></p>
+        <p>Can perform well under pressure<br></br></p>
+        <p>Able to meet deadlines<br></br></p>
+
+        <h1><i class="fas fa-user-graduate"></i> Education</h1>
+        <p>Bcompt Degree - UNISA - 2005</p>
+        <p>Hons Bcompt Degree - UNISA - 2006</p>
+        <p>Full Stack Web Cert - Hyperion Dev - 2019</p>
+        <p>Planning on completing Google Javascript Certification on aquiring the relevant experience</p>
+
+
+
+    </div>
+    
+)
+
+export default details;
